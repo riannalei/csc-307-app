f82207eeb428ba35a39762f6d1ecde709275c147
riannalei <riannalei@gmail.com>
2024-10-03 19:17:33 -0700

    IA 3: Linking Frontend with Backend (Individual Assignment)

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use(cors());
 
 const users = {
   users_list: [
@@ -15,17 +17,11 @@ const users = {
   ]
 };
 
-// Default route for testing
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// GET route to return users, optionally filtered by name or job
+// GET all users, optionally filtered by name or job
 app.get('/users', (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
 
-  // Filter users by name and job if provided
   const filteredUsers = users.users_list.filter((user) => {
     return (!name || user.name === name) && (!job || user.job === job);
   });
@@ -33,14 +29,10 @@ app.get('/users', (req, res) => {
   res.send({ users_list: filteredUsers });
 });
 
-// GET route to find a user by ID
-const findUserById = (id) => {
-  return users.users_list.find((user) => user.id === id);
-};
-
+// GET user by ID
 app.get('/users/:id', (req, res) => {
   const id = req.params.id;
-  const user = findUserById(id);
+  const user = users.users_list.find((user) => user.id === id);
 
   if (user) {
     res.send(user);
@@ -49,33 +41,28 @@ app.get('/users/:id', (req, res) => {
   }
 });
 
-// POST route to add a new user
-const addUser = (user) => {
-  users.users_list.push(user);
-  return user;
-};
-
+// POST to add a new user with a randomly generated ID
 app.post('/users', (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.status(201).send(userToAdd); // Send 201 Created status
+
+  if (!userToAdd.name || !userToAdd.job) {
+    return res.status(400).send("Invalid user data.");
+  }
+
+  // Generate random ID and add user
+  userToAdd.id = Math.random().toString(36).substring(7);
+  users.users_list.push(userToAdd);
+  res.status(201).send(userToAdd);
 });
 
-// DELETE route to remove a user by ID
-const deleteUserById = (id) => {
-  const index = users.users_list.findIndex((user) => user.id === id);
-  if (index !== -1) {
-    users.users_list.splice(index, 1); // Remove user
-    return true;
-  }
-  return false;
-};
-
+// DELETE a user by ID
 app.delete('/users/:id', (req, res) => {
   const id = req.params.id;
-  const success = deleteUserById(id);
-  if (success) {
-    res.status(204).send(); // 204 No Content
+  const index = users.users_list.findIndex((user) => user.id === id);
+
+  if (index !== -1) {
+    users.users_list.splice(index, 1);
+    res.status(204).send();
   } else {
     res.status(404).send("User not found.");
   }
